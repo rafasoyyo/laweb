34ae508f212674bac1a212557d1642a18de10cf8
Rafael <rafa_soyyo@hotmail.com>
2021-04-30 23:06:22 +0200

    version added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.sendfile(__dirname + "/public/index.html");
+});
+
+/* GET app version. */
+router.get('/version', function (req, res, next) {
+    const info = require('../package.json');
+    res.render('index', {
+        title: info.name,
+        version: info.version
+    });
 });
 
 module.exports = router;
