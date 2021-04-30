5f36a0915d206645abb73e2e72f65380dbcb29fb
Rafael <rafa_soyyo@hotmail.com>
2021-05-01 00:02:55 +0200

    helmet config

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(helmet())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"]
+        }
+    }
+}));
 
 app.use('/', require('./routes/index'));
 // app.use('/users', require('./routes/users'));
